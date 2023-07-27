7588505b0c132d70f2564931e4770a0178c32090
E9ex <egecansar@gmail.com>
2023-07-27 14:02:02 +0300

    updatedscripts

--- Assets/scripts/TileRow.cs
+++ Assets/scripts/TileRow.cs
@@ -1,18 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TileRow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public tilecell[] cell { get; private set; }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  private void Awake()
+  {
+    cell = GetComponentsInChildren<tilecell>();
+  }
 }
--- Assets/scripts/tilecell.cs
+++ Assets/scripts/tilecell.cs
@@ -5,4 +5,7 @@ using UnityEngine;
 public class tilecell : MonoBehaviour
 {
    public Vector2Int coordinates { get; set; }
+   public tile Tile { get; set; }
+   public bool empty => Tile == null;
+   public bool occupied => Tile != null;
 }
